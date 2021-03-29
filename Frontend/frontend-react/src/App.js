d6d00eccb2e5a7f5a771e72dfc8e28d88e899ecc
crslegend <risheng17@gmail.com>
2021-03-29 23:54:28 +0800

    push up codes for routing

--- Frontend/frontend-react/src/App.js
+++ Frontend/frontend-react/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import CitizenLoginPage from "../src/citizen/LoginPage";
+import TracerLoginPage from "../src/tracer/LoginPage";
 
 const App = () => {
   return (
     <Switch>
-      <Route exact path="/" component={<div />} />
-      <Route exact path="/tracer" component={<div />} />
-      <Route exact path="/govt" component={<div />} />
+      <Route exact path="/" component={CitizenLoginPage} />
+      <Route exact path="/tracer" component={TracerLoginPage} />
+      <Route exact path="/govt" component={<div></div>} />
     </Switch>
   );
 };
--- Frontend/frontend-react/src/citizen/LoginPage.jsx
+++ Frontend/frontend-react/src/citizen/LoginPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router";
 
 const styles = makeStyles((theme) => ({}));
 
 const LoginPage = () => {
   const classes = styles();
+  const history = useHistory();
 
-  return <div></div>;
+  return <div>HELLO</div>;
 };
 
 export default LoginPage;
