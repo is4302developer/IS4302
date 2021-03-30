87d2c9a434c5ea81f7c54ab96aa94a34179eb8e6
crslegend <risheng17@gmail.com>
2021-03-30 12:44:48 +0800

    added govt login page

--- Frontend/frontend-react/src/App.js
+++ Frontend/frontend-react/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import CitizenLoginPage from "../src/citizen/LoginPage";
 import TracerLoginPage from "../src/tracer/LoginPage";
+import GovtLoginPage from "../src/govt/LoginPage";
 
 const App = () => {
   return (
     <Switch>
       <Route exact path="/" component={CitizenLoginPage} />
       <Route exact path="/tracer" component={TracerLoginPage} />
-      <Route exact path="/govt" component={<div></div>} />
+      <Route exact path="/govt" component={GovtLoginPage} />
     </Switch>
   );
 };
