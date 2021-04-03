8782edfd9dc08f65723a1904fba2729f80df533a
crslegend <risheng17@gmail.com>
2021-04-03 12:31:08 +0800

    correct return type

--- Backend/contracts/Digital_Token.sol
+++ Backend/contracts/Digital_Token.sol
@@ -1,20 +1,19 @@
 pragma solidity ^0.5.0;
 
-import './ERC721Full.sol';
+import "./ERC721Full.sol";
 
 contract Trace_Token is ERC721Full {
-  constructor() ERC721Full("Collectible", "COL") public {}
-  
-  address _owner = msg.sender;
+    constructor() public ERC721Full("Collectible", "COL") {}
 
-  function createToken (bytes32 hashedNric) public {
-    require(msg.sender == _owner);
-  }
-  
-  function getOwnerOfContract() public view returns (address){
-      return _owner;
-  }
-  
-  function checkToken() public returns (boolean) {
-  }
+    address _owner = msg.sender;
+
+    function createToken(bytes32 hashedNric) public {
+        require(msg.sender == _owner);
+    }
+
+    function getOwnerOfContract() public view returns (address) {
+        return _owner;
+    }
+
+    function checkToken() public returns (bool) {}
 }
