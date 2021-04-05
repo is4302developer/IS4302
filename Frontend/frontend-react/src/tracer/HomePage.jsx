c135fa4e4ada5e40620ac787b06db382f235bb68
aeronsimshihwin <aeronsim97@gmail.com>
2021-04-06 02:41:45 +0800

    add tracer homepage

--- Frontend/frontend-react/src/tracer/HomePage.jsx
+++ Frontend/frontend-react/src/tracer/HomePage.jsx
@@ -1,12 +1,131 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Navbar from "../components/Navbar";
 
-const styles = makeStyles((theme) => ({}));
+import {
+  Button,
+  FormControl,
+  TextField,
+  Typography,
+  Grid,
+} from "@material-ui/core";
+import { useHistory } from "react-router";
+import { DataGrid, GridRowsProp, GridColDef } from "@material-ui/data-grid";
 
-const HomePage = () => {
+
+const styles = makeStyles((theme) => ({
+  content: {
+    marginTop: "65px",
+    padding: theme.spacing(7),
+  },
+  formControl: {
+    width: "200px",
+  },
+}));
+
+const HomePage = ({ drizzle, drizzleState }) => {
   const classes = styles();
+  const history = useHistory();
 
-  return <div></div>;
+  const [show, setShow] = useState(false);
+  const [dataKey, setDataKey] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setShow(true);
+  }
+
+  const init = () => {
+    console.log(drizzleState);
+    // const contract = drizzle.contracts.ContactTracing_Token;
+    const contract1 = drizzle.contracts.Trace;
+    // const value = contract.methods["getOwnerOfContract"].cacheCall();
+
+    const txId = contract1.methods["getAccessRecords"].cacheCall(
+      drizzleState.accounts[0],
+      {
+        from: drizzleState.accounts[0],
+      }
+    );
+    console.log(txId);
+    setDataKey(txId);
+  };
+
+  useEffect(() => {
+    init();
+    // setDataKey(value);
+  }, []);
+
+  const safeEntryColumns: GridColDef[] = [
+    { field: "date", headerName: "Date (ddmmyy)", width: 300 },
+    { field: "checkin", headerName: "Check in time (hh:mm:ss)", width: 350 },
+    { field: "checkout", headerName: "Check out time (hh:mm:ss)", width: 350 },
+    { field: "shopname", headerName: "Shops", width: 350 },
+  ];
+
+  // const [safeEntryRows, setSafeEntryRows] = useState([]);
+  const safeEntryRows: GridRowsProp = [
+    { id: 1, date: '250320', checkin: '12:30:01', checkout: '13:05:23', shopname: 'NEX BURGER KING' },
+    { id: 2, date: '250320', checkin: '21:22:15', checkout: '23:58:10', shopname: "KING'S POOL" },
+    { id: 3, date: '260320', checkin: '10:50:30', checkout: '12:16:34', shopname: "TOA PAYOH CENTRAL LIBRARY" },
+    { id: 4, date: '260320', checkin: '15:06:09', checkout: '18:30:48', shopname: "NUS SCHOOL OF COMPUTING" },
+    { id: 5, date: '270320', checkin: '11:30:01', checkout: '12:05:17', shopname: "NUS THE DECK CANTEEN" },
+  ]
+
+  const getDetails = () => {
+    const { Trace } = drizzleState.contracts;
+    console.log(Trace);
+    const getRecords = Trace.getAccessRecords[dataKey];
+    console.log(getRecords);
+  };
+
+  return (
+    <div>
+      <Navbar path="tracer" />
+      <div className={classes.content}>
+        <div>{getDetails()}</div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Grid container alignItems="center">
+            <Grid item>
+              <Typography variant="h6" style={{ paddingRight: "20px" }}>
+                Enter Citizen's hashed NRIC:
+              </Typography>
+            </Grid>
+            <Grid item>
+              <FormControl
+                margin="dense"
+                variant="outlined"
+                className={classes.formControl}
+              >
+                <TextField id="outlined-basic" label="Type here" variant="outlined" required autoFocus/>
+              </FormControl>
+            </Grid>
+
+            <Grid item alignItems="stretch">
+              <Button 
+                variant="contained" 
+                color="primary"
+                type="submit" 
+                onClick={handleSubmit}
+              >
+              Retrieve
+              </Button>
+            </Grid>
+          </Grid>
+        </div>
+        {show && <div style={{ height: "400px", marginTop: "10px" }}>
+          <DataGrid
+            rows={safeEntryRows}
+            columns={safeEntryColumns}
+            pageSize={10}
+            //checkboxSelection
+            disableSelectionOnClick
+            onRowClick={(e) => console.log("E")}
+          />
+        </div>}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
