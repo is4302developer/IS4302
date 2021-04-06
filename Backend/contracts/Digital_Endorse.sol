cb53d687112c20e5ca0f2cbda2bc4d47168556a1
ananda-lye <lyeananda@gmail.com>
2021-04-06 16:55:34 +0800

    updated to check tracer role by Address

--- Backend/contracts/Digital_Endorse.sol
+++ Backend/contracts/Digital_Endorse.sol
@@ -1,16 +1,20 @@
 pragma solidity ^0.5.0;
 import "./Endorsement_Token.sol";
+import "./Digital_Token.sol";
 
 contract DigitalEndorse {
     EndorsementToken tokenContract;
+    ContactTracingToken cttContract;
     address _owner = msg.sender;
     
-    constructor(EndorsementToken contractAddress) public {
+    constructor(EndorsementToken contractAddress, ContactTracingToken cttAddress) public {
         tokenContract = contractAddress;
+        cttContract = cttAddress;
     }
     
     function endorseTracer(address tracer) public {
         //require(tracer is actually a tracer);
+        require(cttContract.isTracerByAddress(tracer));
         require(tokenContract.balanceOf(msg.sender) > 0);
         uint256 tokId = tokenContract.getTokenId(msg.sender);
         tokenContract.approve(address(this), tokId);
